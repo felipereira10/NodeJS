106092c69fe4531b93966d2892a28785d943b61f
Felipe Pereira <felipe.pereira64@fatec.sp.gov.br>
2024-10-24 19:56:04 -0300

    feat: finished

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -29,24 +29,74 @@ exports.Insert = (req, res, next) => {
 
 exports.SearchAll = (req, res, next) => {
     Usuario.findAll()
-    .then(usuario => {
-        if(usuario){
-            res.status(status.OK).send(usuario);
-        }
-    })
-    .catch(error => next(error));
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            }
+        })
+        .catch(error => next(error));
 }
 
 exports.SearchOne = (req, res, next) => {
     const id = req.params.id;
 
     Usuario.findByPk(id)
-    .then(usuario => {
-        if(usuario){
-            res.status(status.OK).send(usuario);
-        }else{
-            res.status(status.NOT_FOUND).send();
-        }
-    })
-    .catch(error => next (error));
+        .then(usuario => {
+            if (usuario) {
+                res.status(status.OK).send(usuario);
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+}
+
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const salario = req.body.salario;
+    const dataNascimento = req.body.dataNascimento;
+    const ativo = req.body.ativo;
+
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.update({
+                    nome: nome,
+                    salario: salario,
+                    dataNascimento: dataNascimento,
+                    ativo: ativo
+                },
+                    {
+                        where: { id: id }
+                    })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+}
+
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+
+    Usuario.findByPk(id)
+        .then(usuario => {
+            if (usuario) {
+                usuario.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error))
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
 }
